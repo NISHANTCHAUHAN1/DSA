f3bcff5218b5f7d009ff2d7774b2f41e2fefffd6
NISHANTCHAUHAN1 <onenish14@gmail.com>
2025-02-25 16:38:10 +0530

    singly done

--- linkedList/start.cpp
+++ linkedList/start.cpp
@@ -41,7 +41,7 @@
 
 
 
-// INSERTION IN LINKED LIST
+// INSERTION IN LINKED LIST AND DELELTION
 
 #include<iostream>
 using namespace std;
@@ -55,6 +55,16 @@ class Node {
     Node(int data){
         this -> data = data;
         this -> next = NULL;
+    }
+
+    // destructor
+    ~Node(){
+        int value = this -> data;
+        if(this -> next != NULL){
+            delete next;
+            this -> next = NULL;
+        }
+        cout << "Memory Free " << value << endl;
     }
 };
 
@@ -97,9 +107,40 @@ void insertAtPos(Node* &tail, Node* &head, int pos, int data){
     NodeToInsert -> next = temp -> next;
     temp -> next = NodeToInsert;
 
-    if(temp -> next == NULL){
-        insertAtTail(tail, data);
-        return;
+    // if(temp -> next == NULL){
+    //     insertAtTail(tail, data);
+    //     return;
+    // }
+    if(NodeToInsert -> next == NULL){ // Update tail if needed
+        tail = NodeToInsert;
+    }
+}
+
+// deletion 
+
+void deleteNode(Node* &head, int pos){
+    if(pos == 1){
+        Node* temp = head;
+        head = head -> next;
+        // memory free
+        temp -> next = NULL;
+        delete(temp);
+    }
+    else {
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int cnt = 1;
+        while(cnt < pos){
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+
+        prev -> next = curr -> next;
+        curr -> next = NULL;
+
+        delete(curr);
     }
 
 }
@@ -130,7 +171,8 @@ int main() {
     insertAtTail(tail, 20);
     insertAtTail(tail, 30);
 
-    insertAtPos(tail, head, 3, 50);
+    // insertAtPos(tail, head, 3, 50);
+    // deleteNode(head, 3);
 
     print(head);
 }
